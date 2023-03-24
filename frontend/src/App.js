356339d29d1d6fd1092cba30bbc8e81181c42ad2
tbeaver23 <tbeaver23@yahoo.com>
2023-03-24 16:51:25 -0500

    update pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import FindATutor from './Pages/FindATutor';
+import Home from './Pages/Home';
+import LogOut from './Pages/LogOut';
+import Notifications from './Pages/Notifications';
+import ProfileSettings from './Pages/ProfileSettings';
 
 function App() {
+  let page
+  switch (window.location.pathname) {
+    case "/":
+        page = <Home/>
+      break
+    case "/FindATutor":
+        page = <FindATutor/>
+      break
+    case "/ProfileSettings":
+        page = <ProfileSettings/>
+      break
+    case "/LogOut":
+        page = <LogOut/>
+      break
+    case "/Notifications":
+        page = <Notifications/>
+      break
+  }
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      {page}
+    </>
   );
 }
 
