fb206b7d46a0696743a07a960c6e0653aa50f8ad
tbeaver23 <tbeaver23@yahoo.com>
2023-03-24 16:22:46 -0500

    update
    navbar

---
+++ frontend/src/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+export default function Navbar(){
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="">Home</a>
+          </li>
+        <li>
+          <a href="">Find A Tutor</a>
+          </li>
+        <li>
+          <a href="">Profile Settings</a>
+          </li>
+        <li>
+          <a href="">Log Out</a>
+          </li>
+        <li>
+          <a href="">Notifications</a>
+          </li>
+      </ul>
+    </nav>
+  )
+}
