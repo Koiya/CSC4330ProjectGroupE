a6ab61de0ec6f950d9f2bd89f8ce200f4d4a1abd
tbeaver23 <tbeaver23@yahoo.com>
2023-03-28 17:14:43 -0500

    added search bar

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,5 +1,10 @@
-
+import SearchBar from "../SearchBar/SearchBar"
 
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+            <h1>Home</h1>
+            <SearchBar/>
+        </>
+    )
 }
--- frontend/src/SearchBar/SearchBar.js
+++ frontend/src/SearchBar/SearchBar.js
@@ -1,12 +1,9 @@
 const SearchBar = () => (
     <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
-        </label>
         <input
             type="text"
             id="header-search"
-            placeholder="Search blog posts"
+            placeholder="Search for tutors"
             name="s" 
         />
         <button type="submit">Search</button>
