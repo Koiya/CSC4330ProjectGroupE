b190ef763855ec175a82b99066c60113ace1b6b4
tbeaver23 <tbeaver23@yahoo.com>
2023-03-28 22:56:31 -0500

    update

--- frontend/src/Pages/NotificationMessage.js
+++ frontend/src/Pages/NotificationMessage.js
@@ -1,7 +1,23 @@
 export default function Notifications() {
     return (
         <>
-        
+            <div className="messages">
+                <p className="messageText">
+                    Student's name
+                </p>
+                <p className="messageText">
+                    Student's name would like to meet at this timeslot
+                </p>
+                <p className="messageText">
+                    for this subject
+                </p>
+                <button>
+
+                </button>
+                <button>
+                    
+                </button>
+            </div>
         </>
     )
 }
