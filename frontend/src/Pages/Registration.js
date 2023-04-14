6b4a38e8f0b6d2b89b650701926f9745bd81f3a1
tbeaver23 <tbeaver23@yahoo.com>
2023-04-13 20:13:34 -0500

    styling update

--- frontend/src/Pages/Registration.js
+++ frontend/src/Pages/Registration.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -48,7 +46,7 @@ export default function SignUp() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: '#282c34' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
