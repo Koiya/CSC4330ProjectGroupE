44aaf0087b5052e79c0c01b514a946dd0a28913c
Daniel Nguyen <Koiya@users.noreply.github.com>
2023-04-25 08:49:30 -0500

    update page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,5 +1,5 @@
 import StarRating from "./components/StarRating";
-import {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {getId, getRole, getToken} from './components/auth';
 import Axios from "axios";
 import DataTable from "./components/datatable";
@@ -7,6 +7,7 @@ import DataTable from "./components/datatable";
 export default function Home(){
     const [num,setNum] = useState('');
     const [data,setData] = useState([]);
+
     let token = getToken();
     const URL = "https://32xcur57b2.execute-api.us-east-2.amazonaws.com/beta/getmessage"
     const getAptURL = "https://32xcur57b2.execute-api.us-east-2.amazonaws.com/beta/getApt"
@@ -37,20 +38,33 @@ export default function Home(){
         (async () => {
             Axios.post(getAptURL,requestBody).then((response) => {
                 setData(response.data);
-                console.log(response.data);
             });
         })();
     }, []);
-
     const columns = useMemo(
+        () => [
+        {
+            Header: 'Student Name',
+            accessor: 'student_name'
+        },
+        {
+            Header: 'Tutor Name',
+            accessor: 'tutor_name'
+        },
+        {
+            Header: 'Expertise',
+            accessor: 'appointment_subject'
+        },
+        {
+            Header: 'Availability',
+            accessor: 'appointment_time'
+        }
+    ],[]);
+    const tutorCol= useMemo(
         () => [
             {
                 Header: 'Student Name',
                 accessor: 'student_name'
-            },
-            {
-                Header: 'Tutor Name',
-                accessor: 'tutor_name'
             },
             {
                 Header: 'Expertise',
@@ -60,22 +74,45 @@ export default function Home(){
                 Header: 'Availability',
                 accessor: 'appointment_time'
             },
-        ],
-        []
-    );
+            {
+                accessor:'request',
+                Cell: ({ row}) => (
+                    <button onClick={ (e) => {
+                        e.preventDefault();
+                        const removeBody = {
+                            tutorID:row.original.tutor_id,
+                            studentID:row.original.student_id,
+                            tutorName:row.original.tutor_name,
+                            time:row.original.appointment_time
+                        }
+                        console.log(removeBody)/*
+                        Axios.post(removeURL,removeBody)
+                            .then( (response) => {
+                                alert(response.data)
+                                navigate(0)
+                            }).catch((err) =>{
+                            setMessage(err.response.data);
+                        })*/
+                    }}>
+                        Done
+                    </button>),
+            },
+        ],[]);
     return (
         <>
         {token ?
                 <>
+        {role === "user" ?
+            <>
             <div className="backgroundSize">
                 <div>
                     <p>Pending appointments: {num} </p>
                     <h2> Upcoming Appointments: </h2>
                     <DataTable data={data} columns={columns}/>
                 </div>
-            </div>{role === "user" ?
-                    <div className="backgroundRating">
-                    <h2> Previous Tutor: </h2>
+            </div>
+            <div className="backgroundRating">
+                <h2> Previous Tutor: </h2>
                     <div className="textAlign">
                         <p>
                             Displaying previous tutor's here
@@ -84,9 +121,18 @@ export default function Home(){
                             <StarRating/>
                         </div>
                     </div>
-                </div>:<></>}
-
-        </>:
+                </div>
+            </>
+            :
+            <div className="backgroundSize">
+                <div>
+                    <h1>Appointment management</h1>
+                    <DataTable data={data} columns={tutorCol}/>
+                </div>
+            </div>
+            }
+        </>
+            :
         <>
         <div className="auth-form-container">
             <h1>Log in to see your appointment</h1>
