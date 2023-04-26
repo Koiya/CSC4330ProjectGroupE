b0e33ea063f24d8518deb25112fd227b3ab748aa
tbeaver23 <tbeaver23@yahoo.com>
2023-04-25 20:04:53 -0500

    update dont have an account link

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -104,7 +104,7 @@ const Login = () => {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link to="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                     </Link>
                   </Grid>
