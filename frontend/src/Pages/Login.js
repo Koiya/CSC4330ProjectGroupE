24f59d55cc6ac2f8b8dafd04049890c15f028676
tbeaver23 <tbeaver23@yahoo.com>
2023-04-25 20:01:43 -0500

    login style update

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,55 +1,118 @@
 import React, { useState } from "react";
-import { useNavigate,Link} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Axios from "axios";
-import {setUserSession} from './components/auth';
+import { setUserSession } from './components/auth';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const Login = () => {
-    let navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [loginStatus, setLoginStatus] = useState('');
-    const URL = "https://32xcur57b2.execute-api.us-east-2.amazonaws.com/beta/login";
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const requestBody = {
-            email:email,
-            password:pass,
-        }
-        Axios.post(URL,requestBody)
-            .then( (response) => {
-                setUserSession(response.data.email, response.data.token, response.data.role, response.data.name,response.data.id);
-                setLoginStatus("Logged in");
-                navigate('/');
-            }).catch((err) =>{
-                console.log(err);
+  let navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
+  const URL = "https://32xcur57b2.execute-api.us-east-2.amazonaws.com/beta/login";
 
-                if(err.response.status === 404){
-                    setLoginStatus(err.response.data);
-                }
-            })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = {
+      email: email,
+      password: pass,
     }
-    
-    return (
-        <div className="auth-form-container">
-            <h1>{loginStatus}</h1>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label className="theLabel" for="email">Email</label>
-                <input className="theInput" value={email}
-                       onChange={
-                        (e) => setEmail(e.target.value)
-                        }
-                       type="email" placeholder="youremail@mail.com" id="email" name="email"/>
+    Axios.post(URL,requestBody)
+      .then((response) => {
+        setUserSession(response.data.email, response.data.token, response.data.role, response.data.name, response.data.id);
+        setLoginStatus("Logged in");
+        navigate('/');
+      }).catch((err) =>{
+        console.log(err);
 
-                <label className="theLabel" for="password">Password</label>
-                <input className="theInput" value={pass}
-                       onChange={
-                        (e) => setPass(e.target.value)
-                        }
-                       type="password" placeholder="********" id="password" name="password"/>
-                <button className="theButton" type="submit">Log In</button>
-             </form>
-            <button className="link-btn"><Link to="/register">Don't have an account? Register here.</Link></button>
-        </div>
-    )
+        if (err.response.status === 404) {
+          setLoginStatus(err.response.data);
+        }
+      })
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      );
 }
 export default Login;
