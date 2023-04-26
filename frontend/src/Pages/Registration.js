0da5781a9058a3e202ba0f561eaa158611a68cfe
tbeaver23 <tbeaver23@yahoo.com>
2023-04-25 23:20:55 -0500

    added user to have to select role

--- frontend/src/Pages/Registration.js
+++ frontend/src/Pages/Registration.js
@@ -24,7 +24,7 @@ export default function Registration() {
     const URL = "https://32xcur57b2.execute-api.us-east-2.amazonaws.com/beta/register";
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(firstName.trim() === '' || email.trim() === ''|| pass.trim() === ''){
+        if(firstName.trim() === '' || email.trim() === ''|| pass.trim() === '' || role === ''){
             setMessage('All fields are required');
             return;
         }
@@ -47,108 +47,108 @@ export default function Registration() {
 
     return (
         <ThemeProvider theme={theme}>
-  <Container component="main" maxWidth="xs">
-    <CssBaseline />
-    <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Typography component="h1" variant="h5">
-        Sign Up
-      </Typography>
-      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              autoComplete="given-name"
-              name="firstName"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              autoFocus
-              value={firstName}
-              onChange={(e) => setFirst(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="family-name"
-              value={lastName}
-              onChange={(e) => setLast(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-            />
-          </Grid>
-          <Grid container justifyContent="center">
-            <Grid item xs={5}>
-              <FormControlLabel
-                control={<Checkbox value="student" color="primary" checked={role === "student"} onChange={(e) => setRole(e.target.value)} />}
-                label="Student"
-              />
-            </Grid>
-            <Grid item xs={5}>
-              <FormControlLabel
-                control={<Checkbox value="tutor" color="primary" checked={role === "tutor"} onChange={(e) => setRole(e.target.value)} />}
-                label="Tutor"
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{
-            mt: 2,
-            mb: 2,
-            backgroundColor: '#1976D2',
-            color: '#fff',
-            '&:hover': {
-              backgroundColor: '#115293',
-            },
-          }}
-        >
-          Sign Up
-        </Button>
-        <Box sx={{ mt: 2, color: '#f44336' }}>
-            {message && <p>{message}</p>}
-        </Box>
-        <Grid container justifyContent="center">
-          <Grid item>
-            <Link to="/login" variant="body2">
-              Already have an account? Sign in
-            </Link>
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-  </Container>
-</ThemeProvider>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography component="h1" variant="h5">
+                    Sign Up
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={firstName}
+                    onChange={(e) => setFirst(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    value={lastName}
+                    onChange={(e) => setLast(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    />
+                </Grid>
+                <Grid container justifyContent="center">
+                    <Grid item xs={5}>
+                        <FormControlLabel
+                            control={<Checkbox color="primary" checked={role === "student"} onChange={(e) => setRole(e.target.checked ? "student" : "")} />}
+                            label="Student"
+                        />
+                    </Grid>
+                    <Grid item xs={5}>
+                        <FormControlLabel
+                            control={<Checkbox color="primary" checked={role === "tutor"} onChange={(e) => setRole(e.target.checked ? "tutor" : "")} />}
+                            label="Tutor"
+                        />
+                    </Grid>
+                </Grid>
+                </Grid>
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                    mt: 2,
+                    mb: 2,
+                    backgroundColor: '#1976D2',
+                    color: '#fff',
+                    '&:hover': {
+                    backgroundColor: '#115293',
+                    },
+                }}
+                >
+                Sign Up
+                </Button>
+                <Box sx={{ mt: 2, color: '#f44336' }}>
+                    {message && <p>{message}</p>}
+                </Box>
+                <Grid container justifyContent="center">
+                <Grid item>
+                    <Link to="/login" variant="body2">
+                    Already have an account? Sign in
+                    </Link>
+                </Grid>
+                </Grid>
+            </Box>
+            </Box>
+        </Container>
+    </ThemeProvider>
     );
 }
