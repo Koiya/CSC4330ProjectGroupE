cdeb392624a365861f15a90b6fef57c92106b0b6
tbeaver23 <tbeaver23@yahoo.com>
2023-04-30 19:12:27 -0500

    removed find tutor page from tutor role

--- frontend/src/Pages/components/Navbar.js
+++ frontend/src/Pages/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar(){
         <nav className="nav">
             <ul>
                 <PageLink to="/">Home</PageLink>
-                <PageLink to="/FindATutor">Find A Tutor</PageLink>
+                {role === "user" ? <><PageLink to="/FindATutor">Find A Tutor</PageLink></>: <></>}
                 {role === "tutor" ? <><PageLink to="/appointment">Appointments</PageLink> <PageLink to="/Notifications">Notifications</PageLink></>: <></>}
             </ul>
             <ul>
